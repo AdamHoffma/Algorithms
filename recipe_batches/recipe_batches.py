fa77bedb7ecd808dc5c1df09b7eeb4e1838dd6a0
Adam Hoffman <wintros@yahoo.com>
2019-12-12 14:36:17 -0700

    recipe_batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,6 +3,13 @@
 import math
 
 def recipe_batches(recipe, ingredients):
+  batches = []
+  if recipe.keys() == ingredients.keys():
+    for key in recipe:
+      batches.append(ingredients[key] // recipe[key])
+      return min(batches)
+  else:
+    return 0
   pass 
 
 
@@ -10,5 +17,5 @@ if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
   # your implementation with different inputs
   recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+  ingredients = { 'milk': 300, 'butter': 150, 'flour': 51 }
   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
